782046d98b6742f8b08fb42982c98d09dc992684
kaism <eigenspaces@gmail.com>
2020-02-17 23:44:45 -0500

    implementing revursive solution

---
+++ python/print_perms.py
@@ -0,0 +1,21 @@
+strg = "abcdefg"
+
+perm = set()
+temp = set()
+
+strl = list(strg)
+newlist = []
+
+a = 1
+
+def perms(strl):
+    return newlist
+
+    if a < lem(strl):
+        l = strl[a::-1]
+        a += 1
+        perms(strl)
+    a += 1
+
+    perms(strl)
+
