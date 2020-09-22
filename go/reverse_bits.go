0d630d5ff9df8a59a3e6f1cdbd9858b210bc4372
kaism <eigenspaces@gmail.com>
2020-09-22 11:04:12 -0400

    solved problem

--- go/reverse_bits.go
+++ go/reverse_bits.go
@@ -1,32 +1,23 @@
 package main
+
 import (
-    "fmt"
+	"fmt"
 )
 
-type stack struct {
-    data int
-    next *int
-    // prev *int ??
-}
-
 func main() {
-    const idx = 12
-    var stack [idx]int
-    var initial = [idx]int{1,1,1,1,0,0,0,0,1,1,2,2}
-    var final [idx]int
-    var limit1 = cap(initial) - 1 // 31
-    var limit2 = cap(final) - 1   // 31
+	const idx = 12
+	// var stack [idx]int
+	var initial = [idx]int{1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 2}
+	var final [idx]int
+	var limit1 = cap(initial) - 1 // 11
+	// var limit2 = cap(final) - 1   // 11
 
-    for i := limit1-1; i >= 0; i-- {
-        stack[i] = initial[limit1 - i]
-        fmt.Println("stack[i] = ", stack[i])
-        fmt.Println("initial[limit1-i] = ", initial[limit1 - i])
-    }
-    for j := ; j <= limit2; j++ {
-        final[limit2 - j] = stack[j]
-    }
+	for i := limit1; i >= 0; i-- {
+		final[i] = initial[limit1-i]
+		fmt.Printf("final[%d] = %d\ninitial[%d] = %d\n", i, final[i], limit1-i, initial[limit1-i])
+	}
 
-    // fmt.Println("initial = ", initial)
-    // fmt.Println("final = ", final)
+	fmt.Println("initial = ", initial)
+	fmt.Println("final = ", final)
 
 }
