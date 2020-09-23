ed868f81261ed0c6a64ed4f381a2e36b2eab833f
kaiwulf <eigenspaces@gmail.com>
2020-09-23 13:13:52 -0400

    working on insertion sort

---
+++ go/insertion-sort.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+ )
+
+func insertionSort(arr []int) []int {
+    for j := 2; j < len(arr); j++ {
+        
+    }
+    return arr
+}
+
+func main() {
+    var a = []int{5,2,4,6,1,3}
+    var b = []int{31,41,59,26,41,58}
+    insertionSort(a)
+    insertionSort(b)
+}
