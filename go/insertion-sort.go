015b70a7d7b4cbd576d9e7ddb4ea6032cc2c94a4
kaiwulf <eigenspaces@gmail.com>
2020-09-24 10:15:42 -0400

    working on insertion sort

--- go/insertion-sort.go
+++ go/insertion-sort.go
@@ -5,8 +5,18 @@ import (
  )
 
 func insertionSort(arr []int) []int {
+    // const capacity = cap(arr)
+    // var sorted [capacity]int
+    // i := 0
     for j := 2; j < len(arr); j++ {
-        
+        key := arr[j]
+        // insert arr[j] into the sorted sequence sorted[1..j-1]
+        // sorted[i] = arr[j]
+        i = j-1
+        for ; i > 0 && arr[i] > key; i-- {
+            arr[i+1] = arr[i]
+        }
+        arr[i+1] = key
     }
     return arr
 }
@@ -14,6 +24,7 @@ func insertionSort(arr []int) []int {
 func main() {
     var a = []int{5,2,4,6,1,3}
     var b = []int{31,41,59,26,41,58}
-    insertionSort(a)
-    insertionSort(b)
+    c := insertionSort(a)
+    d := insertionSort(b)
+    fmt.Println(c, d)
 }
