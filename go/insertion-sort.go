0f094c3118278d55743c0851834bbc78f4be6b39
kaism <eigenspaces@gmail.com>
2020-09-25 20:43:09 -0400

    .

--- go/insertion-sort.go
+++ go/insertion-sort.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-    "fmt"
- )
+	"fmt"
+)
 
 func insertionSort(arr []int) []int {
-    // const capacity = cap(arr)
-    // var sorted [capacity]int
-    // i := 0
-    for j := 2; j < len(arr); j++ {
-        key := arr[j]
-        // insert arr[j] into the sorted sequence sorted[1..j-1]
-        // sorted[i] = arr[j]
-        i = j-1
-        for ; i > 0 && arr[i] > key; i-- {
-            arr[i+1] = arr[i]
-        }
-        arr[i+1] = key
-    }
-    return arr
+	for j := 1; j < len(arr); j++ {
+		key := arr[j]
+		i := j - 1
+		fmt.Println(arr[i], key)
+		for ; i > 0 && arr[i] > key; i-- {
+			// swap := arr[i+1]
+			arr[i+1] = arr[i]
+			// arr[i] = swap
+		}
+		arr[i+1] = key
+	}
+	fmt.Println(arr)
+	return arr
 }
 
 func main() {
-    var a = []int{5,2,4,6,1,3}
-    var b = []int{31,41,59,26,41,58}
-    c := insertionSort(a)
-    d := insertionSort(b)
-    fmt.Println(c, d)
+	var a = []int{5, 2, 4, 6, 1, 3}
+	var b = []int{31, 41, 59, 26, 41, 58}
+	c := insertionSort(a)
+	d := insertionSort(b)
+	fmt.Println(c, d)
 }
