afd2e9caa85a8fedcf656e711a4f294340a27468
kaism <eigenspaces@gmail.com>
2020-09-30 13:55:58 -0400

    bugs

--- go/reverse_bits.go
+++ go/reverse_bits.go
@@ -5,16 +5,19 @@ import (
 )
 
 func main() {
-	const idx = 12
-	// var stack [idx]int
-	var initial = [idx]int{1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 2}
-	var final [idx]int
-	var limit1 = cap(initial) - 1 // 11
-	// var limit2 = cap(final) - 1   // 11
+	var inp int
+	fmt.Println("Enter array limit: ")
+	fmt.Scanf("%d", inp)
 
-	for i := limit1; i >= 0; i-- {
-		final[i] = initial[limit1-i]
-		fmt.Printf("final[%d] = %d\ninitial[%d] = %d\n", i, final[i], limit1-i, initial[limit1-i])
+	const idx = inp
+	// var initial = [idx]int{1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 2}
+	var initial = [idx]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	var final [idx]int
+	var limit = cap(initial) - 1
+
+	for i := limit; i >= 0; i-- {
+		final[i] = initial[limit-i]
+		// fmt.Printf("final[%d] = %d\ninitial[%d] = %d\n", i, final[i], limit-i, initial[limit-i])
 	}
 
 	fmt.Println("initial = ", initial)
